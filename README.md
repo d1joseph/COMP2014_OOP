c57111d577cbf07f4610b82ed7d263c5a3dc90f8
Dhiv Joseph <dhiv.joseph@gmail.com>
2023-08-04 14:23:27 +1000

    docs: added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Object Oriented Programming with C++
+This repository contains all code used for my object oriented programming unit written in C++. Code for practicals, assignments, text book exercises, and sandboxing are stored within their respective folders.
--- practicals/main.cpp
+++ practicals/main.cpp
@@ -6,13 +6,15 @@ using namespace std;
 int main()
 {
     // prac 1 - task 1.1
-    cout << "Welcome to OOP class" << endl; 
+    // cout << "Welcome to OOP class" << endl; 
 
+    // task 1.2
+    // weather();
     
     // task 1.3
     // double dailySales = setDailySales();
     // double payableCommission = calculateCommission(dailySales);
-    //cout << "commission payable: $" << payableCommission << endl;
+    // cout << "commission payable: $" << payableCommission << endl;
 
     // task 1.4
     // printMultiplicationTable();

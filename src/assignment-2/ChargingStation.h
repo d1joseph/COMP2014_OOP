f41c359f2a0c403f08d74c9dd3cb51f5480a10b4
Dhiv Joseph <dhiv.joseph@gmail.com>
2023-09-25 17:03:23 +1000

    feat(ChargingStation): added ChargingStation base class

--- src/assignment-2/ChargingStation.h
+++ src/assignment-2/ChargingStation.h
@@ -1,4 +1,45 @@
 #ifndef CONSTANT_H_
 #define CONSTANT_H_
 
+class ChargingStation {
+private:
+    int cityId;
+    string cityName;
+    int distanceToLastCity;
+    int numberOfChargers;
+public:
+    ChargingStation(
+        int cityId,
+        string cityName,
+        int distanceToLastCity,
+        int numberOfChargers
+    );
+    ~ChargingStation();
+    // getAllAttributes
+    void getAllAttributes();
+};
+
+ChargingStation::ChargingStation(
+    int id,
+    string name,
+    int distance,
+    int numChargers
+) {
+    cityId = id;
+    cityName = name;
+    distanceToLastCity = distance;
+    numberOfChargers = numChargers;
+}
+
+ChargingStation::~ChargingStation() {
+
+}
+
+void ChargingStation::getAllAttributes() {
+    cout << "cityId: " << cityId << endl;
+    cout << "cityName: "<< cityName << endl;
+    cout << "distanceTolastCity: " << distanceToLastCity << endl;
+    cout << "numberOfChargers: " << numberOfChargers << endl;
+}
+
 #endif // CHARGINGSTATION_H_

0a294ff0f37f59ab68f5c9fd80dea2a30090ca27
Dhiv Joseph <dhiv.joseph@gmail.com>
2023-09-25 16:09:15 +1000

    feat(assignment-2): linking class files in main

---
+++ src/assignment-2/main.cpp
@@ -0,0 +1,21 @@
+// Student: Dhivyan Joseph
+// StudentID: 18225899
+// About: COMP2014 OOP Assignment 2 - EV charging schedule system
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include "ChargingAllocation.h"
+#include "ChargingStation.h"
+#include "Constant.h"
+#include "DemandGenerator.h"
+#include "Vehicle.h"
+
+using namespace std;
+
+int main() {
+    cout << "### EV Charging Scheduler v0 ###" << endl;
+}

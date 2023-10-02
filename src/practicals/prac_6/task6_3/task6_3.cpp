6ae653fd706113017df8ebe7e9abd73c8676b1a9
d1joseph <dhiv.joseph@gmail.com>
2023-10-02 19:17:07 +1100

    chore(task 6.3):wip

--- src/practicals/prac_6/task6_3/task6_3.cpp
+++ src/practicals/prac_6/task6_3/task6_3.cpp
@@ -9,7 +9,10 @@
 
 #include <iostream>
 #include <string>
-#include "../../assignment-2/Constant.h"
+#include <fstream>
+#include "../../../assignment-2/Constant.h"
+
+using namespace std;
 
 struct DemandInputFields {
     int VehicleId;
@@ -20,14 +23,48 @@ struct DemandInputFields {
 
 class DemandGenerator {
 private:
-    string outputPath =  "C:Users/dhivj/Dev/Learning/C++/COMP2014_OOP/src/assignment-2/demand/ChargingDemandsSample.txt";
+    static const int defaultLimit = 100;
+    int generationLimit;
+    string outputSavePath; // where to save the output text file
+    ofstream fileOut; // init an fstream object
 public:
-    DemandGenerator(string outputPath, int lineCount) {
+    DemandGenerator(const string& fileName, const string& path = "./", int limit = defaultLimit)
+    : generationLimit(limit), outputSavePath(path + fileName) {        
+        fileOut.open(outputSavePath);
+        cout << "opening file at: " << outputSavePath << endl;
 
+        if (fileOut.is_open()) {
+            for (int i = 0; i < limit; i++) {
+                fileOut << "test line: [" << i + 1 << "]" << endl;
+            }
+        } else {
+            cerr << "error: could not genenerate demand file '" << fileName << "'"
+            << endl;
+        }
+    }
+
+    ~DemandGenerator() {
+        if (fileOut.is_open()) {
+            fileOut.close();
+        }
+
+        cout << "DemandGenerator destructed" << endl;
     }
 };
 
 
 int main() {
+    // test 1 - save the output ChargingDemandsTest.txt
+    string outputFile = "ChargingDemandsTest.txt";
+
+    // test 2 - save the output ChargingsDemandsTest.txt to below output path
+    string outputPath = "C:\\users\\divjoseph\\Desktop\\Dev\\Learning\\C++\\COMP2014_OOP\\src\\assignment-2\\demand\\";
+
+    DemandGenerator dg(
+        outputFile,
+        outputPath,
+        50
+    );
+
     return 0;
 }

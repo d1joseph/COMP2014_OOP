7cb94cdc288775b22920e65f08fa2d497134bfa1
Dhiv Joseph <dhiv.joseph@gmail.com>
2023-10-12 12:49:23 +1100

    chore(assignment 2): added declaration in main.cpp

--- src/assignment-2/main.cpp
+++ src/assignment-2/main.cpp
@@ -1,6 +1,15 @@
-// Student: Dhivyan Joseph
-// StudentID: 18225899
-// About: COMP2014 OOP Assignment 2 - EV charging schedule system
+/*
+Student: Dhivyan Joseph
+StudentID: 18225899
+About: COMP2014 OOP Assignment 2 - EV charging schedule system
+Compiler: g++.exe (Rev7, Built by MSYS2 project) 13.1.0
+
+I hold a copy of this assignment that I can produce if the original is lost or damaged.
+I hereby certify that no part of this assignment has been copied from any other student’s work or
+from any other source except where due acknowledgement is made in the assignment. No part
+of this assignment has been written/produced for me by another person except where such
+collaboration has been authorised by the subject lecturer concerned.
+*/
 
 #include <iostream>
 #include <iomanip>

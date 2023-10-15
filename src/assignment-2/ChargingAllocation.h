29da72aceb3b6ff72b4029d8a27f33f5d031494a
Dhiv Joseph <dhiv.joseph@gmail.com>
2023-10-15 18:34:40 +1100

    feat(assignment 2): added ChargingAllocation class

--- src/assignment-2/ChargingAllocation.h
+++ src/assignment-2/ChargingAllocation.h
@@ -1,4 +1,16 @@
 #ifndef CHARGINGALLOCATION_H_
 #define CHARGINGALLOCATION_H_
 
+class ChargingAllocation {
+private:
+    vector<Vehicle>* vehicles;
+public:
+    ChargingAllocation(vector<Vehicle>* vehicles): vehicles(vehicles) {}
+    void displayVehicleInfo(const Vehicle& vehicle) const;
+};
+
+void ChargingAllocation::displayVehicleInfo(const Vehicle& vehicle) const {
+    vehicle.getVehicleInformation();
+}
+
 #endif // CHARGINGALLOCATION_H_

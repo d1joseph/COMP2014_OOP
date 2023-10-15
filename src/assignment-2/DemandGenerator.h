af512506a49b513e08f2378d42fe2584bcff2224
Dhiv Joseph <dhiv.joseph@gmail.com>
2023-10-15 16:03:08 +1100

    feat(assignment 2): added DemandGenerator class

--- src/assignment-2/DemandGenerator.h
+++ src/assignment-2/DemandGenerator.h
@@ -1,64 +1,45 @@
 #ifndef DEMANDGENERATOR_H_
 #define DEMANDGENERATOR_H_
 
+#include "lib/Utilities.h"
+
 class DemandGenerator {
 private:
-    static const int defaultLimit = MIN_DEMANDS;
-    int generationLimit;
-    string outputSavePath; // where to save the output file
-    ofstream fileOut; // init an fstream object to write the output file
-
-    // generates a random int in a given range
-    int getRandomInt(int min, int max) {
-        random_device rd;
-        mt19937 generator(rd()); // mersene twister engine
-        uniform_int_distribution<int> distribution(min, max);
-
-        return distribution(generator);
-    }
-
-    // generates and returns pseudo random data in a DemandRequest 
-    Vehicle generateDemand(int vehicleId) {
-        Vehicle vehicle;
-
-        vehicle.vehicleId = vehicleId;
-        vehicle.destinationId = getRandomInt(1, NUM_CITIES);
-        vehicle.capacityRange = getRandomInt(MIN_CAPACITY, MAX_CAPACITY);
-        vehicle.remainingRange = getRandomInt(MIN_REMAIN_RANGE, MAX_CAPACITY);
-
-        return vehicle;
-    }
+    int Limit = MAX_DEMANDS;
 public:
-    DemandGenerator(const string& fileName, const string& path = "./", int limit = defaultLimit)
-    : generationLimit(limit), outputSavePath(path + fileName) {        
-        fileOut.open(outputSavePath);
-        cout << "creating demand file at: " << outputSavePath << endl;
-
-        if (fileOut.is_open()) {
-            for (int vehicleId = 1; vehicleId <= limit; vehicleId++) {
-                auto vehicle = generateDemand(vehicleId);
-                fileOut << "[";
-                fileOut << vehicle.VehicleId << ",";
-                fileOut << vehicle.DestinationId << ",";
-                fileOut << vehicle.CapacityRange << ",";
-                fileOut << vehicle.RemainingRange;
-                fileOut << "]" << endl;
-            }
-        } else {
-            cerr << "error: could not genenerate demand file '" << fileName << "'"
-            << endl;
-        }
-
-        cout << "successfully created '" << fileName << "' at: " << outputSavePath << endl;
+    DemandGenerator() {
+        cout << "creating demand file with default limit: " << Limit << endl;
     }
 
-    ~DemandGenerator() {
-        if (fileOut.is_open()) {
-            fileOut.close();
+    DemandGenerator(int& customLimit) {
+        Limit = customLimit;
+        cout << "creating demand file with custom limit: " << Limit << endl;
+    }
+
+    void Generate(string& fileName) {
+        ofstream demandFile(fileName);
+
+        if (!demandFile.is_open()) {
+            cerr << "Unable to open the file.\n";
+            return;
         }
 
-        cout << "DemandGenerator destructed" << endl;
-    }
+        for (int line = 0; line < Limit; line++) {
+            int vehicleId = Limit + line;
+            int destinationId = GenerateRandomInt(1, NUM_CITIES-1);
+            int capacityRange = GenerateRandomInt(MIN_CAPACITY, MAX_CAPACITY);
+            int remainRange = GenerateRandomInt(MIN_REMAIN_RANGE, capacityRange-1);
+
+            demandFile << "[" << vehicleId << "," << destinationId << ","
+                    << capacityRange << "," << remainRange << "]" << endl;
+        }
+
+        if (filesystem::exists(fileName)) {
+            cout << "successfully created: " << fileName << endl;
+        }
+
+        demandFile.close();
+    };
 };
 
 #endif // DEMANDGENERATOR_H_

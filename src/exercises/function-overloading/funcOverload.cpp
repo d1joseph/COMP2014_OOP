8af885aaf0fe7345e7e78bc7eb6fbae6f59d0982
d1joseph <dhiv.joseph@gmail.com>
2023-11-06 10:55:25 +1100

    chore: clean up

--- src/exercises/function-overloading/funcOverload.cpp
+++ src/exercises/function-overloading/funcOverload.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 void DatePrint(int day, int month, int year);
-void DatePrint(int day, char Month, int year);
+void DatePrint(int day, string Month, int year);
 void DatePrint(int month, int day);
 
 int main() {
-    DatePrint(30, "Oct", 2021);
+    string month = "Oct";
+    int monthNum = 10;
+    DatePrint(30, month, 2021);
     return 0;
 }
 
@@ -15,7 +18,7 @@ void DatePrint(int day, int month, int year) {
     cout << "1" << endl;
 }
 
-void DatePrint(int day, char month, int year) {
+void DatePrint(int day, string month, int year) {
     cout << "2" << endl;
 }
 
